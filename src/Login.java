d527809d59a8dc5833e7de37c81d9ab44356ef6e
dmlco1 <dmlco1@iscte-iul.pt>
2021-10-03 18:26:00 +0100

    JPasswordField launch problem fixed by organizing JFrame components by order and in another class

--- src/Login.java
+++ src/Login.java
@@ -17,26 +17,37 @@ public class Login implements ActionListener {
     JFrame frame = new JFrame();
     JButton loginButton = new JButton("Login");
     JTextField idField = new JTextField("ID");
-    JPasswordField passwordField = new JPasswordField("Password");
+    JPasswordField passwordField = new JPasswordField("Password", 10);
 
     HashMap<Integer, String> userInfo;
 
     Login(HashMap<Integer, String> userLogin){
         // Copy of user data that can be accessible for other methods
         userInfo = userLogin;
+        PrepareFrame(); // Create Frame
+        frameComponents(); // Add text fields, labels and buttons
+    }
 
-        // JFrame attributes
+    // Set the JFrame before adding components
+    private void PrepareFrame(){
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
+        frame.setLocationRelativeTo(null); // Always appear in the center of the screen
         frame.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
         frame.setResizable(false);
         frame.setVisible(true);
+    }
+
+    // Add initial components to the Frame
+    private void frameComponents(){
         loginButton.setBounds(100, 150, 100, 50);
         idField.setBounds(100, 50, 100, 30);
-        passwordField.setBounds(100, 100, 100, 30);
         frame.add(loginButton);
         frame.add(idField);
         frame.add(passwordField);
+        // setBounds in passwordField needs to be after add method to work
+        // No clue why this happens
+        passwordField.setBounds(100, 100, 100, 30);
+
     }
 
     @Override
