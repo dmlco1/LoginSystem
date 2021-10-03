2d662882406776037bc7dbc7dc177f2cf653b53e
dmlco1 <dmlco1@iscte-iul.pt>
2021-10-03 17:57:42 +0100

    Main class developed

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
+        IdPassword loginInformation = new IdPassword();
+
+        Login loginPage = new Login(loginInformation.loginInfo);
+    }
 }
