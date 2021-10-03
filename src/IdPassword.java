95a9eac429563e1f66d03f046ed42be5cd9dc6b8
dmlco1 <dmlco1@iscte-iul.pt>
2021-10-03 18:46:56 +0100

    Login and hashmap verification successful. Changed hashmap for both key and object type string

--- src/IdPassword.java
+++ src/IdPassword.java
@@ -3,14 +3,14 @@ import java.util.HashMap;
 
 // Hashmap of every ID and Password
 public class IdPassword {
-    HashMap<Integer, String> loginInfo = new HashMap<>();
+    HashMap<String, String> loginInfo = new HashMap<>();
 
     IdPassword(){
-        loginInfo.put(112, "login");
-        loginInfo.put(555, "soup");
-        loginInfo.put(164, "java");
+        loginInfo.put("112", "login");
+        loginInfo.put("555", "soup");
+        loginInfo.put("164", "java");
     }
-    protected HashMap getInfo(){
+    protected HashMap<String, String> getInfo(){
             return loginInfo;
         }
 }
