4b2c886ef63d13c51e129138027fd39bee688d01
dmlco1 <dmlco1@iscte-iul.pt>
2021-10-03 17:55:55 +0100

    IdPassword class with initial logins

--- src/IdPassword.java
+++ src/IdPassword.java
@@ -1,2 +1,17 @@
+import java.util.HashMap;
+import java.util.HashMap;
+
+// Hashmap of every ID and Password
 public class IdPassword {
+    HashMap<Integer, String> loginInfo = new HashMap<>();
+
+    IdPassword(){
+        loginInfo.put(112, "login");
+        loginInfo.put(555, "soup");
+        loginInfo.put(164, "java");
+    }
+    protected HashMap getInfo(){
+            return loginInfo;
+        }
 }
+
